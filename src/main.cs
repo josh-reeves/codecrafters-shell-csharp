3699ad881a6e738d34ccc57edc8da2742eb9bb4f
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2025-12-20 17:09:24 -0600

    Implemented echo command.

--- src/main.cs
+++ src/main.cs
@@ -1,36 +1,69 @@
 class Program
 {
+    private const char sep = ' ';
     private const string invalidCmdMsg = ": command not found";
 
     static void Main()
     {
         bool replActive = true;
-        string input;
+        string input = string.Empty;
 
-        Dictionary<string, string> commands = new()
+        Dictionary<string, Action> commands = new()
         {
-            {"exit", "exit"}
+            {"exit", () => replActive = false},
+            {"echo", () => echo() }
 
         };
         
+        // Main REPL loop.
         while (replActive)
         {
             Console.Write("$ ");
 
             input = Console.ReadLine() ?? string.Empty;
 
-            if (input == commands["exit"])
-            {
-                replActive = false;
+            string command = input.Split(sep)[0];
 
-                break;
+            if (!commands.Keys.Contains(command))
+            {
+                Console.WriteLine(command + invalidCmdMsg);
+                
+            }
+            else
+            {
+                commands[command]?.Invoke();
 
             }
 
-            Console.WriteLine(input + invalidCmdMsg);
-
         }
 
+#region Local Functions
+        void echo()
+        {
+            string[] inputArray = input.Split(sep);
+
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                Console.Write(inputArray[i]);
+
+                if (i == inputArray.Length - 1)
+                {
+                    Console.Write("\r\n");
+                }
+                else
+                {
+                    Console.Write(sep);
+                    
+                }
+                
+            }
+
+#endregion
+            
+        }
+
+
     }
+
 
 }
