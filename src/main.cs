df9d390bbeed7347e6f5f2e12887197f5a494ff7
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2025-12-20 15:53:40 -0600

    Added basic handling for invalid commands. Currently all commands are treated as invalid.

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,12 @@
 class Program
 {
+    private const string invalidCommandMsg = ": command not found";
+
     static void Main()
     {
         Console.Write("$ ");
+
+        Console.WriteLine(Console.ReadLine() + invalidCommandMsg);
 
     }
 
