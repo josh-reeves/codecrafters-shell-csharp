3999777eee6e5cd51ec77c348473a204ad7d7230
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2025-12-21 02:38:14 -0600

    't'

--- src/shell.cs
+++ src/shell.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.IO.Enumeration;
+
 namespace Shell;
 
 class Shell
@@ -58,14 +61,29 @@ class Shell
 
             input = Console.ReadLine()?.Split(cmdSep) ?? [ string.Empty ];
 
-            if (!Commands.Keys.Contains(input[0]))
+            if (Commands.Keys.Contains(input[0]))
             {
-                Console.WriteLine(input[0] + invalidCmdMsg);
-                
+                 Commands[input[0]]?.Invoke();
+               
+            }
+            else if (IsExecutable(input[0], out string path))
+            {
+                Process process = new();
+
+                process.StartInfo.FileName = path;
+
+                for (int i = 1; i < input.Length; i ++ )
+                {
+                    process.StartInfo.ArgumentList.Add(input[i]);
+
+                }
+
+                process.Start();
+
             }
             else
             {
-                Commands[input[0]]?.Invoke();
+                Console.WriteLine(input[0] + invalidCmdMsg);
 
             }
 
@@ -113,44 +131,77 @@ class Shell
             
         }
 
-        foreach(string dir in PathList)
+        if (IsExecutable(input[1], out string path))
         {
-            string file = dir + dirSep + input[1];
+            Console.WriteLine(input[1] + " is " + path);
 
-            if (FileIsExecutable(file))
-            {
-                Console.WriteLine(input[1] + " is " + file );
-
-                return;
-                
-            }
-
+            return;
+            
         }
 
-        Console.WriteLine(input[1] + cmdNotFoundMsg);
-            
+        Console.WriteLine(input[1] + cmdNotFoundMsg);   
         
     }
 
-    private bool FileIsExecutable(string file)
+    private bool IsExecutable(string fileName)
     {
-        if (!File.Exists(file))
+        foreach(string dir in PathList)
         {
-            return false;
+            string file = dir + dirSep + fileName;
 
-        }
-
-        if (!OperatingSystem.IsWindows())
-        {
-            string fileMode = File.GetUnixFileMode(file).ToString().ToLower();
-
-            if (fileMode.Contains("execute"))
+            if (!File.Exists(file))
             {
-                return true;
+                continue;
+
+            }
+
+            if (!OperatingSystem.IsWindows())
+            {
+                string fileMode = File.GetUnixFileMode(file).ToString().ToLower();
+
+                if (fileMode.Contains("execute"))
+                {
+                    return true;
+                    
+                }
                 
             }
-            
+
         }
+
+        return false;
+    
+    }
+
+    private bool IsExecutable(string fileName, out string path)
+    {
+        foreach(string dir in PathList)
+        {
+            string file = dir + dirSep + fileName;
+
+            if (!File.Exists(file))
+            {
+                continue;
+
+            }
+
+            if (!OperatingSystem.IsWindows())
+            {
+                string fileMode = File.GetUnixFileMode(file).ToString().ToLower();
+
+                if (fileMode.Contains("execute"))
+                {
+                    path = file;
+
+                    return true;
+                    
+                }
+                
+            }
+
+        }
+
+        path = string.Empty;
 
         return false;
     
